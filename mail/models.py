ecb8ec99b87207bb2310de944753a7cb5120e6fb
Kevin Jin <bobkameron@gmail.com>
2021-12-15 16:37:09 -0600

    made a few edits to the database

--- mail/models.py
+++ mail/models.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
-
 
 class Email(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="emails")
